a64db9b56e9021fb8eb0ce32a847b52c62618faf
Geonsu Lee <legonsu2@gmail.com>
2023-10-02 16:39:48 +0900

    init/geonsu/최초커밋

---
+++ src/main/java/com/back/uniHair/UniHairApplication.java
@@ -0,0 +1,13 @@
+package com.back.uniHair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniHairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniHairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/uniHair/UniHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.uniHair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniHairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
